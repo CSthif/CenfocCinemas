3ff95ed08100cba3089f4e7caf255b0405612083
CtrlStif <Sthifarroyo@gmail.com>
2025-06-16 12:40:54 -0600

    Tester

--- DevTester/Program.cs
+++ DevTester/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DataAccess.DAOs;
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        var sqlOperation = new SqlOperation();
+        sqlOperation.ProcedureName = "CRE_USER_PR";
+
+        sqlOperation.AddStringParameter("P_UserCode", "Ctrlsthif");
+        sqlOperation.AddStringParameter("P_Name", "Sthif");
+        sqlOperation.AddStringParameter("P_Email", "Sthifaroyo.ch@gmail.com");
+        sqlOperation.AddStringParameter("P_Password", "Sthif123");
+        sqlOperation.AddStringParameter("P_Status", "AC");
+        sqlOperation.AddDateTimeParam("P_BirthDate", DateTime.Now);
+
+        var sqlDao = SqlDao.GetInstance();
+
+        sqlDao.ExecuteProcedure(sqlOperation);
+    }
+}
